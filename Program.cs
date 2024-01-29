2e7514ddf2e62dcd033300a82e4e48a71107ff9d
Cangoo65 <caneraytekin06@hotmail.com>
2024-01-29 19:49:47 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+var ay = DateTime.Now.Month;
+string ayad;
+switch (ay)
+{
+    case 1:
+        ayad = "Ocak";
+        break;
+    case 2:
+        ayad = "Şubat";
+        break;
+    case 3:
+        ayad = "Mart";
+        break;
+    case 4:
+        ayad = "Nisan";
+        break;
+    case 5:
+        ayad = "Mayıs";
+        break;
+    default:
+        ayad = "Diğer Aylar";
+        break;
+}
+Console.WriteLine("Şimdi " + ayad + " ayında bulunuyorsunuz.");
